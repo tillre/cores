073940fd5464e0f58710a4caf1ce0b454867ed3a
Till Reitemeyer <till@skoni.de>
2013-05-08 14:20:01 +0200

    use core assert instead of chai

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 /*global before after beforeEach afterEach describe it*/
 
-var expect = require('chai').expect;
 var async = require('async');
 var nano = require('nano')('http://localhost:5984');
 var cores = require('../index.js');
+
+var assert = require('assert');
+var util = require('util');
 
 
 describe('cores', function() {
@@ -45,12 +47,13 @@ describe('cores', function() {
 
   var res = null;
 
+  
   describe('Resource creation', function() {
 
     it('should create with schema', function(done) {
       createResource({ name: resName, schema: schema }, function(err, r) {
-        expect(err).to.not.exist;
-        expect(r).to.be.a('object');
+        assert(!err);
+        assert(typeof r === 'object');
         done();
       });
     });
@@ -58,7 +61,7 @@ describe('cores', function() {
 
     it('should not create without name', function(done) {
       createResource({ schema: schema, design: design, hooks: hooks }, function(err, r) {
-        expect(err).to.exist;
+        assert(util.isError(err));
         done();
       });
     });
@@ -66,7 +69,7 @@ describe('cores', function() {
     
     it('should not create with invalid schema', function(done) {
       createResource({ name: resName, schema: {bar:42} }, function(err, r) {
-        expect(err).to.exist;
+        assert(util.isError(err));
         done();
       });
     });
@@ -74,7 +77,7 @@ describe('cores', function() {
 
     it('should not create with invalid design', function(done) {
       createResource({ name: resName, schema: schema, design: {views:''} }, function(err, r) {
-        expect(err).to.exist;
+        assert(util.isError(err));
         done();
       });
     });
@@ -85,32 +88,33 @@ describe('cores', function() {
           createOption: 'create', loadOption: 'load', saveOption: 'save' },
 
         function(err, r) {
-          expect(err).to.not.exist;
-          expect(r).to.be.a('object');
+          assert(!err);
+          assert(typeof r === 'object');
 
           res = r;
 
-          expect(res.create).to.be.a('function');
-          expect(res.load).to.be.a('function');
-          expect(res.save).to.be.a('function');
-          expect(res.destroy).to.be.a('function');
-          expect(res.view).to.be.a('function');
-
+          assert(typeof res.create === 'function');
+          assert(typeof res.load === 'function');
+          assert(typeof res.save === 'function');
+          assert(typeof res.destroy === 'function');
+          assert(typeof res.view === 'function');
+          
           // schema should have a name added
-          expect(res.schema.name).to.equal('Article');
+          assert(res.schema.name === 'Article');
           
           done();
         }
       );
     });
 
+    
     it('should upload design and schema to db', function(done) {
       db.get('_design/' + res.design.name, function(err, doc) {
-        expect(err).to.not.exist;
-        expect(doc.views).to.be.a('object');
-        expect(doc.views.all).to.exist;
-        expect(doc.views.titles).to.exist;
-        expect(doc.schema).to.be.a('object');
+        assert(!err);
+        assert(typeof doc.views === 'object');
+        assert(doc.views.all);
+        assert(doc.views.titles);
+        assert(typeof doc.schema === 'object');
         done();
       });
     });
@@ -123,18 +127,19 @@ describe('cores', function() {
 
     it('should create a document', function(done) {
       res.create(function(err, d) {
-        expect(err).to.not.exist;
-        expect(d).to.be.a('object');
-        expect(res.checkType(d)).to.not.exist;
+        assert(!err);
+        assert(typeof d === 'object');
+        assert(!res.checkType(d));
         done();
       });
     });
 
+    
     it('should create a document with data', function(done) {
       res.create(data, function(err, d) {
-        expect(err).to.not.exist;
-        expect(d).to.be.a('object');
-        expect(res.checkType(d)).to.not.exist;
+        assert(!err);
+        assert(typeof d === 'object');
+        assert(!res.checkType(d));
 
         doc = d;
         
@@ -142,76 +147,86 @@ describe('cores', function() {
       });
     });
 
+    
     it('should not validate data without required properties', function(done) {
       res.validate({ type_: 'Article' }, function(err) {
-        expect(err).to.exist;
+        assert(util.isError(err));
         done();
       });
     });
 
+    
     it('should validate with required properties', function(done) {
       res.validate(data, function(err) {
-        expect(err).to.not.exist;
+        assert(!err);
         done();
       });
     });
+
     
     it('should not save when not valid', function(done) {
       res.save({ type_: 'Article' }, function(err) {
-        expect(err).to.exist;
+        assert(util.isError(err));
         done();
       });
     });
 
+    
     it('should save when valid', function(done) {
       res.save(doc, function(err, d) {
-        expect(err).to.not.exist;
-        expect(d).to.be.a('object');
-        expect(d._id).to.be.a('string');
-        expect(d._rev).to.be.a('string');
+        assert(!err);
+        assert(typeof d === 'object');
+        assert(typeof d._id === 'string');
+        assert(typeof d._rev === 'string');
         done();
       });
     });
 
+    
     it('should save when updated', function(done) {
       doc.title = 'Some other title';
       res.save(doc, function(err, d) {
-        expect(err).to.not.exist;
-        expect(d).to.be.a('object');
-        expect(d._id).to.equal(doc._id);
-        expect(d._rev).to.equal(doc._rev);
+        assert(!err);
+        assert(typeof d === 'object');
+        assert(d._id === doc._id);
+        assert(d._rev === doc._rev);
         done();
       });
     });
 
+    
     it('should not save when has wrong type', function(done) {
       res.save({ _id: 'somefoo', type_: 'Foo' }, function(err, d) {
-        expect(err).to.exist;
+        assert(util.isError(err));
         done();
       });
     });
+
     
     it('should load', function(done) {
       res.load(doc._id, function(err, d) {
-        expect(err).to.not.exist;
-        expect(d.title).to.equal(doc.title);
+        assert(!err);
+        assert(d.title === doc.title);
         done();
       });
     });
 
+    
     it('should have the properties from the hooks', function(done) {
       res.load(doc._id, function(err, d) {
-        expect(err).to.not.exist;
-        expect(d.createHook).to.equal('create');
-        expect(d.loadHook).to.equal('load');
-        expect(d.saveHook).to.equal('save');
+        assert(!err);
+        assert(d.createHook === 'create');
+        assert(d.loadHook === 'load');
+        assert(d.saveHook === 'save');
+        
         done();
       });
     });
 
+    
     it('should destroy', function(done) {
       res.destroy(doc, function(err) {
-        expect(err).to.not.exist;
+        assert(!err);
         done();
       });
     });
@@ -250,9 +265,9 @@ describe('cores', function() {
     
     it('should call the all view with no params', function(done) {
       res.view('all', function(err, docs) {
-        expect(err).to.not.exist;
-        expect(docs).to.be.a('object');
-        expect(docs.total_rows).to.equal(numDocs);
+        assert(!err);
+        assert(typeof docs === 'object');
+        assert(docs.total_rows === numDocs);
         done();
       });
     });
@@ -260,35 +275,37 @@ describe('cores', function() {
 
     it('should call the all view with params', function(done) {
       res.view('all', { limit: 2  }, function(err, docs) {
-        expect(err).to.not.exist;
-        expect(docs).to.be.a('object');
-        expect(docs.total_rows).to.equal(numDocs);
-        expect(docs.rows.length).to.equal(2);
+        assert(!err);
+        assert(docs.total_rows === numDocs);
+        assert(docs.rows.length === 2);
         done();
       });
     });
 
+    
     it('should call the titles view', function(done) {
       res.view('titles', function(err, docs) {
-        expect(err).to.not.exist;
-        expect(docs).to.be.a('object');
-        expect(docs.total_rows).to.equal(numDocs);
+        assert(!err);
+        assert(typeof docs === 'object');
+        assert(docs.total_rows === numDocs);
         done();
       });
     });
 
+    
     it('should call the titles view with params', function(done) {
       res.view('titles', { limit: 1 }, function(err, docs) {
-        expect(err).to.not.exist;
-        expect(docs.total_rows).to.equal(numDocs);
-        expect(docs.rows.length).to.equal(1);
+        assert(!err);
+        assert(docs.total_rows === numDocs);
+        assert(docs.rows.length === 1);
         done();
       });
     });
 
+    
     it('should respond with error when view does not exist', function(done) {
       res.view('foo', function(err, docs) {
-        expect(err).to.exist;
+        assert(util.isError(err));
         done();
       });
     });
